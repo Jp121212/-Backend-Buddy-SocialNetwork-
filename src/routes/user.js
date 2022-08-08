8a097d9d0dd892a42445c17cd08394656e9ad51c
Jp121212 <jose.elizondo@unadeca.net>
2022-08-08 15:25:08 -0600

    Update db
    Hola

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const prisma = new PrismaClient();
 var { expressjwt: jwt } = require("express-jwt");
 var jwt1 = require('jsonwebtoken');
-const secretKey = process.env.SECRET_KEY;
+const secretKey = "8ae350cc8b9ae710042d44353d7551f662a3b340a382e30b878670a5ec287a13e182d71aa9a1b93183b14bd742955b58b860f6c4b2b65c74a90d27bf683b5a50fae7344dbb004f3a954c00d689f1df0d4bdd5e4e339782759008591a893cfb7de8cb9b239de0b99815b2b68b129987a80893bbdea98e4ac6fb3a2b6fffaa68";
 
 router.use(cors());
 router.use(express.json());
